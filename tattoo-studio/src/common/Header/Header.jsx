3781b9d4ff339fbe3162c2d08827fccc9dd5e4e9
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-11-09 19:20:16 +0100

    feat: add header to the whole web page

---
+++ tattoo-studio/src/common/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import "./Header.css"
+
+export const Header = () => {
+    
+    return (
+        <div className='headerDesign'>
+           
+        </div>
+    )
+}
