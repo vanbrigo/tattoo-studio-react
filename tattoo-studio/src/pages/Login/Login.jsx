4dd0a9fb8a95e8ba0b87fcba34f4c9431ec29ba8
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-11-10 16:12:09 +0100

    feat: add validations for login's inputs

--- tattoo-studio/src/pages/Login/Login.jsx
+++ tattoo-studio/src/pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import { Input } from '../../common/Input/Input'
 import { Button } from '../../common/Button/Button'
 import { useNavigate } from 'react-router-dom'
 import { logUser } from '../../services/apiCalls'
-
+import { validator } from '../../services/validations'
 
 export const Login = () => {
     const navigate = useNavigate()
@@ -14,6 +14,11 @@ export const Login = () => {
         password:""
     })
 
+    const [credencialesError,setCredencialesError] =useState({
+        emailError:"",
+        passwordError:""
+    })
+
     
     const functionHandler = (e) => {
         setCredenciales((prevState)=>({
@@ -21,6 +26,19 @@ export const Login = () => {
             [e.target.name]:e.target.value
         }))
     }
+
+    const errorCheck = (e) => {
+
+        let error = "";
+    
+        error = validator(e.target.name, e.target.value);
+    
+        setCredencialesError((prevState) => ({
+            ...prevState,
+            [e.target.name + 'Error']: error,
+        }));
+      }
+
     const logIn =() =>{
         logUser(credenciales)
         .then(
@@ -36,19 +54,23 @@ export const Login = () => {
             <Input
                 name={"email"}
                 type={"email"}
-                style={"loginInput"}
+                style={`loginInput ${credencialesError.emailError !== "" ? 'inputError' : ''}`}
                 lenght={"30"}
                 placeholder={"email"}
                 functionProp={functionHandler}
+                functionCheck={errorCheck}
             />
+            <div className='errorText'>{credencialesError.emailError}</div>
             <Input
                 name={"password"}
                 type={"password"}
-                style={"loginInput"}
+                style={`loginInput ${credencialesError.passwordError !== "" ? 'inputError' : ''}`}
                 lenght={"30"}
                 placeholder={"password"}
                 functionProp={functionHandler}
+                functionCheck={errorCheck}
             />
+            <div className='errorText'>{credencialesError.passwordError}</div>
             <Button
             style={"loginButton"}
             functionToDo={logIn}
