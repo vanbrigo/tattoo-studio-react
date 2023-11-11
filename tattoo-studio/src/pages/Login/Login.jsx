04952c63b42e95437521a546fdcc72bc9ec29dd7
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-11-12 00:30:40 +0100

    style:add new colors to login view

--- tattoo-studio/src/pages/Login/Login.jsx
+++ tattoo-studio/src/pages/Login/Login.jsx
@@ -51,6 +51,7 @@ export const Login = () => {
 
     return (
         <div className="loginDesign">
+            <div className='loginBox'>
             <Input
                 name={"email"}
                 type={"email"}
@@ -76,6 +77,7 @@ export const Login = () => {
             functionToDo={logIn}
             title={"Log in"}
             />
+            </div>
         </div>
     )
 }
