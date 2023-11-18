ba0849c5a62ae3263820c01de49c4740ede6a7d0
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-11-18 10:13:00 +0100

    style:add react bootstrap for a responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './common/Header/Header'
 import { Body } from './pages/Body/Body'
 
 export default function App() {
-  return <div className='appDesign'>
+  return <div>
     <Header/>
     <Body />
   </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,35 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import './Home.css'
 
 export const Home = () => {
     return (
-        <div className="homeBackground">
-            <div className='homeDesign'></div>
-            <div className='homeDesign2'>
+        <Container className="homeBackground">
+        
+            <Row className='homeDesign'></Row>
+            <Col className='homeDesign2 text-center'>
                 We are a contemporary custom tattoo studio located in Valencia. <br />We are Spain’s first female-only staffed studio.<br />
                 Yes, we tattoo men too!
-            </div>
-            <div className='homeBox'>
-                <img src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im3.jpg'></img>
-                <img src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im2.jpg'></img>
-                <img src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im3.jpg'></img>
-                <img src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im4.jpg'></img>
-                <img src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im5.jpg'></img>
-            </div>
-            <div className='homeDesign2 bigger'>
+            </Col>
+            <Row className='homeBox'>
+                <Col sm={12} lg={6} xl={2} xxl={2} className='boxImage'>
+                <img className='imageBox' src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im3.jpg'></img>
+                </Col>
+                <Col sm={12} lg={6} xl={2} xxl={2} className='boxImage d-none d-md-block'>
+                <img className='imageBox' src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im2.jpg'></img>
+                </Col>
+                <Col sm={12} lg={6} xl={2} xxl={2} className='boxImage d-none d-md-block'>
+                <img className='imageBox' src='https://www.estudiotatuajesmadrid.com/wp-content/uploads/2021/05/im4.jpg'></img>
+                </Col>
+            </Row>
+            <Row className='homeDesign2 text-center'>
+                <Col>
                 We are also one of only few studios in London to have been awarded 4/4 <br />star rating by the Tattoo Hygiene Rating Scheme.<br />
                 Our studio is designed and created by female tattoo artists. <br />Tattoo stations are fitted with hygienic, purpose-built equipment. <br />
                 We operate a completely disposable system <br />so that we have no need for scrub rooms or re-sterilised tools. <br />
                 All of this makes Velvet Underground Tattoo<br /> a reassuringly clean and comfortable environment to be inked.
-            </div>
-        </div>
+                </Col>
+            </Row>
+        
+        </Container>
     )
 }
