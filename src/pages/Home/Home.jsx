055b589fe6ceca037888f62d24cea391ff060aea
Vanessa Brito <vanbrigo@Air-de-Vanessa.home>
2023-11-19 11:11:41 +0100

    style: add timetable and location to home view

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,12 @@ import './Home.css'
 export const Home = () => {
     return (
         <Container fluid className="homeBackground">
-        
+            <Row className='socialMedias'>
+                <Col sm={2} lg={2}>
+                <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/FFFFFF/facebook-f.png" alt="facebook-f"/>
+                <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/FFFFFF/instagram-new--v1.png" alt="instagram-new--v1"/>
+                </Col>
+            </Row>
             <Row className='homeDesign'></Row>
             <Col className='homeDesign2 text-center'>
                 We are a contemporary custom tattoo studio located in Valencia. <br />We are Spain’s first female-only staffed studio.<br />
@@ -41,6 +46,14 @@ export const Home = () => {
             </Col>
             </Row>
             <Row className='contactHome'>
+                <Col sm={12} md={4} lg={4} xl={4} xxl={4}>
+                    
+                </Col>
+                <Col sm={12} md={4} lg={4} xl={4} xxl={4} className='informationBox'>
+                <div><img width="26" height="26" src="https://img.icons8.com/metro/26/FFFFFF/clock--v1.png" alt="clock--v1"/> Monday—Saturday: 10:00-12:00 / 14:00-17:00</div><div><img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/FFFFFF/external-delivery-pin-for-parcel-delivery-location-making-delivery-bold-tal-revivo.png" alt="external-delivery-pin-for-parcel-delivery-location-making-delivery-bold-tal-revivo"/> Plaza Virgen de la Paz, 3, 46001 Valencia</div><div><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/FFFFFF/phone-not-being-used.png" alt="phone-not-being-used"/> 961603000</div>
+                </Col>
+                <Col sm={12} md={4} lg={4} xl={4} xxl={4} className='locationBox'>
+                </Col>
 
             </Row>
         
